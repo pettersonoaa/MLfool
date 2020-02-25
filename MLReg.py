c30bae7927b97d1d2dc3a51837b020d741063425
FEIÃO! <petterson.oaa@gmail.com>
2020-02-25 18:33:29 -0300

    Create MLReg.py

---
+++ MLReg.py
@@ -0,0 +1,4 @@
+https://towardsdatascience.com/choosing-a-scikit-learn-linear-regression-algorithm-dd96b48105f5
+https://github.com/qlanners/scikit-learn_disect/blob/master/all_model_tester.ipynb
+
+
