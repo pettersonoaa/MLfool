80296a1da96eaf48a5b474228c3d9bc1a676b30a
POAA <petterson.oaa@gmail.com>
2020-03-13 18:48:00 -0300

    validação ok

--- setup_logit.py
+++ setup_logit.py
@@ -1,47 +1,52 @@
 import numpy as np, pandas as pd, matplotlib.pyplot as plt
 
-def fit_logit (X, y, X_label, y_label, report=True, test_size=0.2, random_state=31):
+def fit_logit (X, y, report=True, test_size=0.2, random_state=31):
   
-  # train and test split
-  from sklearn.model_selection import train_test_split
-  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=radom_state)
+    # train and test split
+    from sklearn.model_selection import train_test_split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
 
-  # load model and parms
-  from sklearn.linear_model import LogisticRegression
-  parm = [0.0001, 0.001, 0.01, 0.1, 1, 10, 100, 1000, 10000]
-  train_acc = test_acc = model = np.empty(len(parm))
-  max_score_index = 0
+    # load model and parms
+    from sklearn.linear_model import LogisticRegression
+    parms = [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1, 5, 10, 50, 100, 500, 1000, 5000, 10000]
+    train_acc = test_acc = np.empty(len(parms))
+    max_score_index = 0
 
-  # get best parm
-  for i in enumerate(parm):
-    model[i] = LogisticRegression(c=parm[i]).fit(X_train, y_train)
-    train_acc[i], test_acc[i] = model.score(X_train, y_train), model.score(X_test, y_test)
-    if test_acc[max_score_index] < test_acc[i]:
-      max_score_index = i
+    # get best parms
+    for i, k in enumerate(parms):
+        model = LogisticRegression(C=k).fit(X_train, y_train)
+        train_acc[i], test_acc[i] = model.score(X_train, y_train), model.score(X_test, y_test)
+        if test_acc[max_score_index] < test_acc[i]:
+            max_score_index = i
+            best_model = model
 
-  # show report and plots
-  if report:
-    plt.plot(parms, train_acc, label='Train')
-    plt.plot(parms, test_acc, label='Test')
-    plt.legend()
-    plt.show()
+    # show report and plots
+    if report:
+        plt.plot(train_acc, label='Train')
+        plt.plot(test_acc, label='Test')
+        plt.legend()
+        plt.show()
 
-    y_pred = model[max_score_index].predict(X_test)
-    y_prob = model[max_score_index].predict_proba(X_test)[:, 1]
+        y_pred = best_model.predict(X_test)
+        y_prob = best_model.predict_proba(X_test)[:, 1]
 
-    from sklearn.metrics import confusion_matrix, classification_report, roc_curve, roc_auc_score
-    print(confusion_matrix(y_test, y_pred))
-    print(classification_report(y_test, y_pred)
+        from sklearn.metrics import confusion_matrix, classification_report, roc_curve, roc_auc_score
+        print(confusion_matrix(y_test, y_pred))
+        print(classification_report(y_test, y_pred))
 
-    fpr, tpr, threshholds = roc_curve(y_test, y_prob)
-    auc = roc_auc_score(y_test, y_prob)
-    plt.plot([0, 1], [0, 1], 'k--')
-    plt.plot(fpr, tpr, label='AUC: '+str(auc))
-    plt.show()
-  
-  return model[max_score_index]
+        fpr, tpr, threshholds = roc_curve(y_test, y_prob)
+        auc = roc_auc_score(y_test, y_prob)
+        plt.plot([0, 1], [0, 1], 'k--')
+        plt.plot(fpr, tpr, label='AUC: '+str(auc))
+        plt.show()
+
+    return best_model
 
 #load dataset
+#from sklearn.datasets import load_iris
+#data = pd.DataFrame(load_iris().data  , columns=load_iris().feature_names, index=load_iris().target).reset_index()
+#y = data[data['index'] != 2]['index']
+#X = data[data['index'] != 2].drop(['index'], axis=1)
 df = pd.read_csv('xxx.csv')
 
 # set X and y vars
