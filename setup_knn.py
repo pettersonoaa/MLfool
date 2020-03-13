dcb85cf1671531483e0ed7c1d61a920755d0cfcb
POAA <petterson.oaa@gmail.com>
2020-03-13 16:55:57 -0300

    att func with max_k

--- setup_knn.py
+++ setup_knn.py
@@ -1,20 +1,25 @@
 import numpy as np, pandas as pd, matplotlib.pyplot as plt
 
-def fit_knn (X, y, X_label, y_label, report=True):
+def fit_knn (X, y, X_label, y_label, max_k=10, report=True):
+  
+  # train and test split
   from sklearn.model_selection import train_test_split
   X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=31)
 
+  # load model and parms
   from sklearn.neighbors import KNeighborsClassifier
-  neighbors = np.arange(1, np.sqrt(len(X.columns)))
+  neighbors = np.arange(1, max_k)
   train_acc = test_acc = knn = np.empty(len(neighbors))
   max_score = max_score_index = 0
 
+  # get best k
   for i, k in enumerate(neighbors):
     knn[i] = KNeighborsClassifier(n_neighbors=k).fit(X_train, y_train)
     train_acc[i], test_acc[i] = knn.score(X_train, y_train), knn.score(X_test, y_test)
     if max_score < test_acc[i]:
       max_score, max_score_index = test_acc[i], i
 
+  # show report and plots
   if report:
     plt.plot(neighbors, train_acc, label='Train')
     plt.plot(neighbors, test_acc, label='Test')
@@ -39,5 +44,9 @@ def fit_knn (X, y, X_label, y_label, report=True):
 #load dataset
 df = pd.read_csv('xxx.csv')
 
+# set X and y vars
 X_label, y_label = [], []
 X, y = df[X_label], df[y_label]
+          
+# fit the model
+fit_knn(X, y, X_label, y_label)
